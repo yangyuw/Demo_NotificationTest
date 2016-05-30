9a3c8da0f46df2661dc309a5c227a50ea3dad1c0
Yu Yang <yu.yang2@hpe.com>
2016-05-30 09:14:12 +0800

    notification

---
+++ app/src/main/java/com/andrew/demo_notificationtest/NotificationActivity.java
@@ -0,0 +1,16 @@
+package com.andrew.demo_notificationtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by andrew on 5/30/16.
+ */
+public class NotificationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification_layout);
+    }
+}
